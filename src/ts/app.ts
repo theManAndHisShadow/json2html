140124154f1b6ba2be9a49071d754434120e70e6
plvs vltra <justemailpostbox@gmail.com>
2023-03-10 00:04:08 +0500

    Added render() function, now app renders after theme switching

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -13,34 +13,43 @@ function getSelectedThemeName(){
 
 
 
-function init(){
-    let defaultJSONString = `{
-    "string":"Hello world!", "paragraph":"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo?", "link":"https://www.freedesktop.org/wiki/","number":42, "negativeNumber": -1,"floatNumber":3.1415926535,"boolean":true, "isNull": null, "isUndefined": "undefined", "emptyArray": [], "emptyObject": {}, "arrayOfNumbers": [1, 2, 3, 4, 5], "arrayOfObjects": [{"id": 1, "profileType":"public","blocked": false}, {"id":2, "profileType":"private", "blocked": true}, {"id": 3, "profileType":"private", "blocked": false}], "superNested": { "level1": {"level2": {"level3": {"level4":{"level5":{"level6":"Btw I use Arch"}}}}}}
-}`;
-
-    let textArea: HTMLTextAreaElement = document.querySelector('#app #app__input textarea');
-    textArea.textContent = defaultJSONString;
-
-    let defaultFormatted = json2html({
-        json: defaultJSONString,
+/**
+ * Renders text as parsed and formatted JSON.
+ * @param text 
+ */
+function render(text: string){
+    let formatted = json2html({
+        json: text,
         collapseAll: false,
         showTypeOnHover: true,
         theme: getSelectedThemeName(),
     });
 
-    updateOutput(defaultFormatted);
+    updateOutput(formatted);
+}
 
-    // update formatted JSON on textarea changing
+
+
+function init(){
+    let defaultJSONString = `{
+    "string":"Hello world!", "paragraph":"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo?", "link":"https://www.freedesktop.org/wiki/","number":42, "negativeNumber": -1,"floatNumber":3.1415926535,"boolean":true, "isNull": null, "isUndefined": "undefined", "emptyArray": [], "emptyObject": {}, "arrayOfNumbers": [1, 2, 3, 4, 5], "arrayOfObjects": [{"id": 1, "profileType":"public","blocked": false}, {"id":2, "profileType":"private", "blocked": true}, {"id": 3, "profileType":"private", "blocked": false}], "superNested": { "level1": {"level2": {"level3": {"level4":{"level5":{"level6":"Btw I use Arch"}}}}}}
+}`;
+
+    let themeSelector:HTMLSelectElement = document.querySelector('#controls__theme-selector') as HTMLSelectElement;
+    let textArea: HTMLTextAreaElement = document.querySelector('#app #app__input textarea');
+    textArea.textContent = defaultJSONString;
+   
+    // render on init
+    render(defaultJSONString);
+
+    // render on theme changing
+    themeSelector.addEventListener('change', event => {
+        render(textArea.value);
+    });
+
+    // render formatted JSON on textarea changing
     textArea.addEventListener('keyup', (event) => {
-
-        let formatted = json2html({
-            json: textArea.value,
-            collapseAll: false,
-            showTypeOnHover: true,
-            theme: getSelectedThemeName(),
-        });
-
-        updateOutput(formatted);
+        render(textArea.value);
     });
 }
 
