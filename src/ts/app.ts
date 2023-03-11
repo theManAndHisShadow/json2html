4e15aa2e4dea82f0792fcd70463dc11f676e0a50
plvs vltra <justemailpostbox@gmail.com>
2023-03-11 09:59:35 +0500

    Removed getThemeColors() function, added preloadThemeFiles() function as theme loading bug fix

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,94 +1,97 @@
 import { json2html } from './lib/json2html';
 
 
+interface Collection {
+    [key: string]: {
+        [key: string]: string, 
+    };
+}
 
-/**
- * Returns theme colors object.
- * @param themeName name of theme
- * @returns object of theme colors, for example {propColor: hex}
- */
-function getThemeColors(themeName: string){
-    interface Collection {
-        [key: string]: {
-            [key: string]: string, 
-        };
-    }
+const themes:Collection= {
+    dracula: {
+        background: '#21232c',
+        tile: '#282a36',
+        border: '#424242',
+        foreground: '#ffffff',
+        error: '#ee5d44',
+    },
 
-    const themes:Collection = {
-        dracula: {
-            background: '#21232c',
-            tile: '#282a36',
-            border: '#424242',
-            foreground: '#ffffff',
-            error: '#ee5d44',
-        },
+    monokai: {
+        background: '#22231e',
+        tile: '#272822',
+        border: '#424242',
+        foreground: '#ffffff',
+        error: '#fd971f',
+    },
 
-        monokai: {
-            background: '#22231e',
-            tile: '#272822',
-            border: '#424242',
-            foreground: '#ffffff',
-            error: '#fd971f',
-        },
+    daylight: {
+        background: '#e7e7e7',
+        tile: '#ffffff',
+        border: '#b6b6b6',
+        foreground: '#000000',
+        error: '#bf0404',
+    },
 
-        daylight: {
-            background: '#e7e7e7',
-            tile: '#ffffff',
-            border: '#b6b6b6',
-            foreground: '#000000',
-            error: '#bf0404',
-        },
+    horizon: {
+        background: '#ded5cc',
+        tile: '#ffffff',
+        border: '#b6b6b6',
+        foreground: '#2f2f2f',
+        error: '#bf0404',
+    },
 
-        horizon: {
-            background: '#ded5cc',
-            tile: '#ffffff',
-            border: '#b6b6b6',
-            foreground: '#2f2f2f',
-            error: '#bf0404',
-        },
+    "github-light": {
+        background: '#e7e7e7',
+        tile: '#ffffff',
+        border: '#cacdd1',
+        foreground: '#25292f',
+        error: '#bf0404',
+    },
 
-        "github-light": {
-            background: '#e7e7e7',
-            tile: '#ffffff',
-            border: '#cacdd1',
-            foreground: '#25292f',
-            error: '#bf0404',
-        },
+    "github-dark": {
+        background: '#10161d',
+        tile: '#0d1117',
+        border: '#30373d',
+        foreground: '#c9d1d9',
+        error: '#cd3c3c',
+    },
 
-        "github-dark": {
-            background: '#10161d',
-            tile: '#0d1117',
-            border: '#30373d',
-            foreground: '#c9d1d9',
-            error: '#cd3c3c',
-        },
+    "gruvbox-dark": {
+        background: '#242424',
+        tile: '#282828',
+        border: '#3f3f3f',
+        foreground: '#A89984',
+        error: '#bf241d',
+    },
 
-        "gruvbox-dark": {
-            background: '#242424',
-            tile: '#282828',
-            border: '#3f3f3f',
-            foreground: '#A89984',
-            error: '#bf241d',
-        },
+    "gruvbox-light": {
+        background: '#e1d8b2',
+        tile: '#FBF1C7',
+        border: '#b7a5a0',
+        foreground: '#7C6F64',
+        error: '#bf241d',
+    },
 
-        "gruvbox-light": {
-            background: '#e1d8b2',
-            tile: '#FBF1C7',
-            border: '#b7a5a0',
-            foreground: '#7C6F64',
-            error: '#bf241d',
-        },
+    andromeda:{
+        background: '#1e2127',
+        tile: '#23262e',
+        border: '#444444',
+        foreground: '#adb2bc',
+        error: '#bf241d',
+    },
+}
 
-        andromeda:{
-            background: '#1e2127',
-            tile: '#23262e',
-            border: '#444444',
-            foreground: '#adb2bc',
-            error: '#bf241d',
-        },
-    }
 
-    return themes[themeName];
+function preloadThemeFiles(){
+    let allThemeNames = Object.keys(themes);
+
+    allThemeNames.forEach(theme => {
+        let preloadedCSS = document.createElement('link');
+        preloadedCSS.setAttribute('rel', 'preload');
+        preloadedCSS.setAttribute('href', `css/themes/${theme}.css`);
+        preloadedCSS.setAttribute('as', 'style');
+        document.head.appendChild(preloadedCSS);
+    });
 }
 
 
@@ -110,7 +113,7 @@ function getSelectedThemeName(){
  * @param themeName 
  */
 function changeAppTheme(themeName: string){
-    let themeColors = getThemeColors(themeName);
+    let themeColors = themes[themeName];
 
     let appContainer: HTMLDivElement = document.querySelector('#app');
     let appInput: HTMLTextAreaElement = document.querySelector('#app #app__input');
@@ -153,7 +156,7 @@ function toggleErrorMessage(state: boolean, message?: string){
     if(state === true) {
         let errorMessage = `[Error! ${message}]:`;
         let themeName = getSelectedThemeName();
-        let themeColors = getThemeColors(themeName);
+        let themeColors = themes[themeName];
         errorMessageConainer.textContent = errorMessage;
         errorMessageConainer.style.display = 'initial';
         errorMessageConainer.style.color = themeColors.error;
@@ -213,6 +216,8 @@ function renderText(text: string){
 
 
 function init(){
+    preloadThemeFiles();
+
     let defaultJSONString = `{
     "string":"Hello world!", "paragraph":"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo?", "link":"https://www.freedesktop.org/wiki/","number":42, "negativeNumber": -1,"floatNumber":3.1415926535,"boolean":true, "isNull": null, "isUndefined": "undefined", "emptyArray": [], "emptyObject": {}, "arrayOfNumbers": [1, 2, 3, 4, 5], "arrayOfObjects": [{"id": 1, "profileType":"public","blocked": false}, {"id":2, "profileType":"private", "blocked": true}, {"id": 3, "profileType":"private", "blocked": false}], "superNested": { "level1": {"level2": {"level3": {"level4":{"level5":{"level6":"Btw I use Arch"}}}}}}
 }`;
