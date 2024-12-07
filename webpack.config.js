1264ef280a1f1eafae4de6ae05e939e2a1995eb2
theManAndHisShadow <justemailpostbox@gmail.com>
2024-12-07 12:52:21 +0500

    [Misc]: minor changes.

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,8 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: {
-      'lib/json2html': './src/ts/lib/core/json2html.ts',
-      'app': './src/ts/demo/app.ts',
+        'lib/json2html': './src/ts/lib/core/json2html.ts',
+        'app': './src/ts/demo/app.ts',
     },
 
     mode: 'devlopment',
@@ -14,38 +14,39 @@ module.exports = {
     module: {
         // Use `ts-loader` on any file that ends in '.ts'
         rules: [
-          {
-            test: /\.ts$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-          },
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
         ],
-      },
-      // Bundle '.ts' files as well as '.js' files.
-      resolve: {
+    },
+    // Bundle '.ts' files as well as '.js' files.
+    resolve: {
         extensions: ['.ts', '.js'],
-      },
+    },
 
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: 'js/[name].js',
     },
 
-    plugins:[
-      new HtmlWebpackPlugin({
-        template: './src/index.html',
-        inject: false,
-      }),
-      new CopyWebpackPlugin({
-        patterns: [{ 
-          from: "./src/css/", 
-          to: "./css/",
-          globOptions: {
-            // this line helps avoid compile final dist with samples folder
-            ignore: [
-                '**/css/samples/**',
-            ]
-          }}],
-      }),
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            inject: false,
+        }),
+        new CopyWebpackPlugin({
+            patterns: [{
+                from: "./src/css/",
+                to: "./css/",
+                globOptions: {
+                    // this line helps avoid compile final dist with samples folder
+                    ignore: [
+                        '**/css/samples/**',
+                    ]
+                }
+            }],
+        }),
     ],
 }
