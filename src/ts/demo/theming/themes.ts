2ec6d43a17f8a8b8bc1d26ff69d94d2db656ce17
theManAndHisShadow <justemailpostbox@gmail.com>
2024-12-07 13:07:11 +0500

    [Lib]: changed lib themes file extension.

--- src/ts/demo/theming/themes.ts
+++ src/ts/demo/theming/themes.ts
@@ -1,39 +1,39 @@
 // Theme imports for app + renderes json text 
 
 // Andromeda themes
-import andromedaAppTheme from './andromeda.theme.js';
+import andromedaAppTheme from './andromeda.theme';
 import andromedaTheme from '../../lib/theming/themes/andromeda.theme';
 
 // Dracula themes
-import draculaAppTheme from './dracula.theme.js';
+import draculaAppTheme from './dracula.theme';
 import draculaTheme from '../../lib/theming/themes/dracula.theme';
 
 // Daylight themes
-import daylightAppTheme from './daylight.theme.js';
+import daylightAppTheme from './daylight.theme';
 import daylightTheme from '../../lib/theming/themes/daylight.theme';
 
 // Horizon themes
-import horizonAppTheme from './horizon.theme.js';
+import horizonAppTheme from './horizon.theme';
 import horizonTheme from '../../lib/theming/themes/horizon.theme';
 
 // Github Light themes
-import githubLightAppTheme from './githubLight.theme.js';
+import githubLightAppTheme from './githubLight.theme';
 import githubLightTheme from '../../lib/theming/themes/githubLight.theme';
 
 // Github Dark themes
-import githubDarkAppTheme from './githubDark.theme.js';
+import githubDarkAppTheme from './githubDark.theme';
 import githubDarkTheme from '../../lib/theming/themes/githubDark.theme';
 
 // Gruvbox Light themes
-import gruvboxLightAppTheme from './gruvboxLight.theme.js';
+import gruvboxLightAppTheme from './gruvboxLight.theme';
 import gruvboxLightTheme from '../../lib/theming/themes/gruvboxLight.theme';
 
 // Grubbox Dark themes
-import gruvboxDarkAppTheme from './gruvboxDark.theme.js';
+import gruvboxDarkAppTheme from './gruvboxDark.theme';
 import gruvboxDarkTheme from '../../lib/theming/themes/gruvboxDark.theme';
 
 // Monokai themes
-import monokaiAppTheme from './monokai.theme.js';
+import monokaiAppTheme from './monokai.theme';
 import monokaiTheme from '../../lib/theming/themes/monokai.theme';
 
 
